6dbbb7cc9d3222e0a58d7909202d21745f66087f
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-25 10:27:00 +0800

    > compile NEMU ysyx_22041090 刘新 Linux ubuntu 5.13.0-52-generic #59-Ubuntu SMP Wed Jun 15 20:17:13 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:27:00 up  2:08,  1 user,  load average: 0.03, 0.03, 0.00

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -20,7 +20,7 @@ run-env: $(BINARY) $(DIFF_REF_SO)
 
 run: run-env
 	$(call git_commit, "run NEMU")
-	$(NEMU_EXEC)
+	$(NEMU_EXEC)||(echo "quit" ; exit 1)  
 
 gdb: run-env
 	$(call git_commit, "gdb NEMU")
