64ff572dc625890f69462d4f87efaaa2add186fa
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-19 08:24:23 +0800

    > compile NEMU ysyx_22041090 刘新 Linux ubuntu 5.13.0-52-generic #59-Ubuntu SMP Wed Jun 15 20:17:13 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:24:23 up 1 day, 12:54,  1 user,  load average: 0.28, 0.32, 0.28

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -9,6 +9,10 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
+    for(int i = 0; i <= 31; i++){
+        printf("Unknown command '%s'\n", arg);
+        
+    }
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
