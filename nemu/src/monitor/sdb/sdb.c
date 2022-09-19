e6d5ef70498dcd29b211f6d5a7bd7d14b92461f7
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-19 11:08:44 +0800

    > compile NEMU ysyx_22041090 刘新 Linux ubuntu 5.13.0-52-generic #59-Ubuntu SMP Wed Jun 15 20:17:13 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:08:44 up 1 day, 15:39,  1 user,  load average: 0.25, 0.52, 0.45

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -48,6 +48,8 @@ static int cmd_si(char *args){
 
 static int cmd_info(char *args){
     if(*args == 'r')isa_reg_display();
+    if(*args == 'w');
+    else printf("Unknown command '%s'\n", args);
     return 0;
 }
 
