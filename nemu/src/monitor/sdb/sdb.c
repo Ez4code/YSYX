b3411f35639f93c0ed21543bc31e8e9e35f45a7b
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-20 16:55:58 +0800

    > compile NEMU ysyx_22041090 刘新 Linux ubuntu 5.13.0-52-generic #59-Ubuntu SMP Wed Jun 15 20:17:13 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:55:58 up 2 days, 21:26,  1 user,  load average: 0.08, 0.10, 0.18

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -56,7 +56,7 @@ static int cmd_x(char * args, char * sub_args){
     printf("addr = %lx\n",addr);
     for(int i = 0; i < atoi(args); i++){
         word_t mem = vaddr_read(addr + i * 4, 4);
-        printf("%-15lx:%lx\n", addr + i * 4, mem);
+        printf("%-15lx: %-14lx\n", addr + i * 4, mem);
 
     }
     return 0;
