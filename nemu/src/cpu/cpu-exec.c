32e1cc85096bf149061b680e88bb0152da09071b
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-20 10:30:38 +0800

    > compile NEMU ysyx_22041090 刘新 Linux ubuntu 5.13.0-52-generic #59-Ubuntu SMP Wed Jun 15 20:17:13 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:30:38 up 2 days, 15:01,  1 user,  load average: 0.24, 0.23, 0.23

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -96,6 +96,7 @@ void assert_fail_msg() {
 
 /* Simulate how the CPU works. */
 void cpu_exec(uint64_t n) {
+    printf("n:%ld",n);
   g_print_step = (n < MAX_INST_TO_PRINT);
   switch (nemu_state.state) {
     case NEMU_END: case NEMU_ABORT:
