f17368fe2ebd197a90328254cc9c848dd046c7ba
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-26 11:52:14 +0800

    > compile NEMU ysyx_22041090 刘新 Linux ubuntu 5.13.0-52-generic #59-Ubuntu SMP Wed Jun 15 20:17:13 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:52:14 up 4 days, 19:13,  1 user,  load average: 0.81, 1.11, 0.94

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -88,7 +88,7 @@ static bool make_token(char *e) {
          */
 
         switch (rules[i].token_type) {
-          case TK_NOTYPE: tokens[nr_token ++].type = TK_NOTYPE; break;     //weather space need nr_token ++?
+          case TK_NOTYPE: break;
           case '+': tokens[nr_token ++].type = '+'; break;
           case '-': tokens[nr_token ++].type = '-'; break;
           case '*': tokens[nr_token ++].type = '*'; break;
