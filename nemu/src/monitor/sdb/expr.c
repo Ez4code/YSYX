51beaea91755c2d84e02c58d7ae74da27d817347
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-15 08:09:06 +0800

    > compile NEMU ysyx_22041090 刘新 Linux ubuntu 5.13.0-52-generic #59-Ubuntu SMP Wed Jun 15 20:17:13 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:09:06 up 5 days,  5:20,  1 user,  load average: 0.48, 0.22, 0.08

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -156,10 +156,10 @@ static bool make_token(char *e) {
             minus_flag = false;
             break;
           case TK_DEREF:
-            if (substr_len > 32) assert(0);                                //debug
-            vaddr_t addr_value = strtol(substr_start ,NULL, 16);
-            uint32_t mem = vaddr_read(addr_value, 4);
-            sprintf(tokens[nr_token].str,"%u",mem);
+//            if (substr_len > 32) assert(0);                                //debug
+//            vaddr_t addr_value = strtol(substr_start ,NULL, 16);
+//            uint32_t mem = vaddr_read(addr_value, 4);
+//            sprintf(tokens[nr_token].str,"%u",mem);
             tokens[nr_token ++].type = TK_NUM;
             plus_flag = false;
             minus_flag = false;
