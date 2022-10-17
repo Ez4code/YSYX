842e97beb8783ea7e3e414a95b395799adc62412
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-17 19:51:05 +0800

    > compile NEMU ysyx_22041090 刘新 Linux ubuntu 5.13.0-52-generic #59-Ubuntu SMP Wed Jun 15 20:17:13 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:51:05 up 7 days, 17:01,  1 user,  load average: 0.85, 1.06, 0.93

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -4,6 +4,8 @@
 
 typedef struct watchpoint {
   int NO;
+  int value;
+  bool flag;
   struct watchpoint *next;
 
   /* TODO: Add more members if necessary */
@@ -18,6 +20,7 @@ void init_wp_pool() {
   for (i = 0; i < NR_WP; i ++) {
     wp_pool[i].NO = i;
     wp_pool[i].next = (i == NR_WP - 1 ? NULL : &wp_pool[i + 1]);
+    wp_pool[i].flag = false;
   }
 
   head = NULL;
@@ -26,3 +29,52 @@ void init_wp_pool() {
 
 /* TODO: Implement the functionality of watchpoint */
 
+
+WP* new_wp(int value){
+  if(free_ == NULL){
+    printf("ERROR, wp_pool is full!");
+    assert(0);
+  }
+  WP *new = NULL;
+  new = free_;
+  free_ = free_->next;
+  new->value = value;
+  new->next = NULL;
+  if(head == NULL) head = new;
+  else{
+    new->next = head;//insert behind
+    head = new;
+  }
+  return head;
+}
+
+void free_wp(int num){
+  if(head == NULL){
+    printf("ERROR, wp_pool is empty");
+    return;
+  }
+  WP *p = head;
+  while(p->NO != num){
+    p = p->next;
+    if (p->next == NULL){
+      printf("ERROR, No %d watchpoint!",num);
+      return;
+    }
+  }
+  free_ = p;
+  free_->next = free_;
+}
+
+void print_wp(){
+  WP *p = head;
+  if(head == NULL){
+    printf("ERROR, wp is empty!");
+    return;
+  }
+  while(p != NULL){
+    printf("%d  0x%08x\n", p->NO, p->value);
+    p = p->next;
+  }
+  return;
+
+}
